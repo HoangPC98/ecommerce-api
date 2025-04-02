b7aea50c6deb14d9274587722692bc9a7392b67d
hoangpc <anhgog1198@gmail.com>
2025-04-02 17:16:07 +0700

    update code 2025/04/02: infra docker

--- infra/run_docker.sh
+++ infra/run_docker.sh
@@ -3,6 +3,15 @@
 docker network create --driver bridge ecom_common
 
 docker volume create postgres
+docker volume create redis
+docker volume create rabbitmq
+docker volume create mongodb
 
+# docker compose -f ./docker/docker-compose.db.yml up -d
 docker compose -f ./docker/docker-compose.yml up -d
-docker compose -f ./docker/docker-compose.db.yml up -d
+
+docker compose -f ../ecom-gateway-api/docker-compose.yml up -d
+docker compose -f ../ecom-client-api/docker-compose.yml up -d
+docker compose -f ../ecom-messaging/docker-compose.yml up -d
+
+# tsproto --path ./ecom-protos-grpc --output ./ecom-client-api/src/interfaces/protos
