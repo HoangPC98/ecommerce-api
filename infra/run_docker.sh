34ac8c5a1734621067f84c0f654b50b7159be913
hainh <hainh@fabbi.io>
2025-04-22 00:25:08 +0700

    update 22-04-2025 for deploy aws

--- infra/run_docker.sh
+++ infra/run_docker.sh
@@ -8,8 +8,8 @@ docker volume create mongodb
 # docker compose -f ./docker/docker-compose.db.yml up -d
 docker compose -f ./docker/docker-compose.yml up -d
 
-# docker compose -f ../ecom-gateway-api/docker-compose.yml up -d
-# docker compose -f ../ecom-client-api/docker-compose.yml up -d
-# docker compose -f ../ecom-messaging/docker-compose.yml up -d
+docker compose -f ../ecom-gateway-api/docker-compose.yml up -d
+docker compose -f ../ecom-client-api/docker-compose.yml up -d
+docker compose -f ../ecom-messaging/docker-compose.yml up -d
 
 # tsproto --path ./ecom-protos-grpc --output ./ecom-client-api/src/interfaces/protos
